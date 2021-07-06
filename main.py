d8a16e53a79ee4e53fcae0a35504341176c2c776
PedroLucca <pedrolucca27@gmail.com>
2021-07-05 21:33:54 -0300

    Programa finalizado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from perceptron import Perceptron
+import argparse
 
 variaveis = []
 novo_array = []
@@ -9,5 +10,22 @@ for i in variaveis:
     novo_array.append(i.split(" "))
 variaveis = novo_array
 
-pc = Perceptron(2, variaveis)
+parser = argparse.ArgumentParser(description='Receber os pesos do algoritmo.')
+parser.add_argument('peso1', metavar='peso1', type=float)
+parser.add_argument('peso2', metavar='peso2', type=float)
+parser.add_argument('peso3', metavar='peso3', type=float)
+
+args = parser.parse_args()
+pesos_aux = [args.peso1, args.peso2, args.peso3]
+
+taxa = input("Digite a taxa de aprendizado: ")
+
+pc = Perceptron(2, variaveis, pesos_aux, float(taxa))
 pc.algoritmo()
+
+print("--- Finalizado ---\n")
+print("Testando valores: \n")
+print("Resultado(0,0): ", pc.predict(0,0))
+print("Resultado(0,1): ", pc.predict(0,1))
+print("Resultado(1,0): ", pc.predict(1,0))
+print("Resultado(1,1): ", pc.predict(1,1))
--- perceptron.py
+++ perceptron.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 class Perceptron:
-    def __init__(self, n, variaveis):
+    def __init__(self, n, variaveis, pesos_aux, taxa):
         self.n = n
-        self.pesos = [-0.3, 0.5, 0.6]
+        self.pesos = pesos_aux
         self.variaveis = variaveis
+        self.parada = 0
+        self.taxa = taxa
 
     def algoritmo(self):
         self.x_var = []
         self.y_var = []
         self.pesos = np.array(self.pesos)
+        self.k = 0
 
         for i in range(0, self.n+2):
             for j in range(0, self.n+2):
                 self.variaveis[i][j] = float(self.variaveis[i][j])
 
         passo = 0
-        for k in range(0,200):
+        while True:
             self.x_var = self.variaveis[passo][:-1]
             self.y_var = self.variaveis[passo][-1]
             
@@ -37,16 +40,44 @@ class Perceptron:
             erro = self.y_var - net_aux
             
             for i in range(0,self.n+1):
-                self.pesos[i] = self.pesos[i] + 0.1*erro*self.x_var[i]
+                self.pesos[i] = self.pesos[i] + self.taxa*erro*self.x_var[i]
+
+        
+            resultado = self.pesos[0]*self.x_var[0] + self.pesos[1]*self.x_var[1] + self.pesos[2]*self.x_var[2]
+
+            if resultado >= 0:
+                resultado = 1
+            else:
+                resultado = 0
+
+            if resultado == self.y_var:
+                self.parada = self.parada + 1
+            else:
+                self.parada = 0
 
             passo = passo + 1
-
+            
             if passo >= 4:
                 passo = 0
-
-            print("-------------\n")
-            print("Entradas", self.x_var)
-            print("Alvo", self.y_var)
-            print("Net", net_aux)
             
-        print("--- Finalizou ---\n Pesos:", self.pesos)
+            self.k = self.k + 1
+
+            print("** Iteração : ", self.k)
+            print("Entradas: ", self.x_var)
+            print("Erro: ", erro)
+            print("Pesos: ", self.pesos)
+            print("-------------\n")
+
+            if self.parada == 4:
+                break
+            
+        
+
+    def predict(self, x, y):
+        u = self.pesos[0]*1 + self.pesos[1]*x + self.pesos[2]*y
+        if u >= 0:
+            u = 1
+        else:
+            u = 0
+        
+        return u
